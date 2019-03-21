c269641f16311d87b3100fa012e1e46111ea6422
menfre <13710414456@163.com>
2019-03-21 22:46:38 +0800

    add enum

--- generator/src/main/java/org/mendora/generate/px/PxPojoTypeSpec.java
+++ generator/src/main/java/org/mendora/generate/px/PxPojoTypeSpec.java
@@ -1,13 +1,14 @@
 package org.mendora.generate.px;
 
-import com.squareup.javapoet.FieldSpec;
-import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.*;
 import org.mendora.config.AnnotationConfig;
+import org.mendora.db.TableDesc;
 import org.mendora.generate.base.AbstractPojoTypeSpec;
 import org.mendora.util.StringUtils;
 
 import javax.lang.model.element.Modifier;
 import java.util.Comparator;
+import java.util.Optional;
 
 /**
  * @author menfre
@@ -16,6 +17,55 @@ import java.util.Comparator;
  * desc:
  */
 public class PxPojoTypeSpec extends AbstractPojoTypeSpec {
+
+    private void buildEnum(TableDesc td, TypeSpec.Builder pojoBuilder) {
+        String enumName0 = StringUtils.lineToHump(td.field());
+        String enumName = StringUtils.firstLetterToUpperCase(enumName0);
+        // 构造枚举
+        TypeSpec.Builder enumBuilder = TypeSpec.enumBuilder(enumName)
+                .addModifiers(Modifier.PUBLIC)
+                .addField(int.class, "val", Modifier.PUBLIC, Modifier.FINAL)
+                .addField(String.class, "msg", Modifier.PUBLIC, Modifier.FINAL)
+                .addMethod(MethodSpec.constructorBuilder()
+                        .addParameter(int.class, "val")
+                        .addParameter(String.class, "msg")
+                        .addStatement("this.$N = $N", "val", "val")
+                        .addStatement("this.$N = $N", "msg", "msg")
+                        .build());
+
+        String comment = td.comment();
+        int index = comment.indexOf("(");
+        String substring = comment.substring(index + 1, comment.length() - 1);
+        comment = comment.substring(0, index);
+        String[] status = substring.split("\\|");
+
+        for (int i = 0; i < status.length; i++) {
+            String[] str = status[i].split(":");
+            String name = str[0].trim();
+            int val = Integer.valueOf(str[1].trim());
+            String msg = str[2].trim();
+            TypeSpec.Builder builder = TypeSpec.anonymousClassBuilder(val + ", \"" + msg + "\"");
+            if (0 == i) {
+                builder.addJavadoc(comment);
+            }
+            enumBuilder.addEnumConstant(name, builder.build());
+        }
+
+        ParameterizedTypeName returnType = ParameterizedTypeName.get(Optional.class, enumBuilder.build().getClass());
+        MethodSpec methodSpec = MethodSpec.methodBuilder("valOf").addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .addParameter(ParameterSpec.builder(int.class, "val").build())
+                .returns(returnType)
+                .addCode("\tfor ($N $N : values()){\n", enumName, enumName0)
+                .addCode("\t\tif(val == status.val){\n")
+                .addStatement("\t\t\treturn Optional.of($N)", enumName0)
+                .addCode("\t\t}\n")
+                .addCode("\t}\n")
+                .addStatement("\treturn Optional.empty()")
+                .build();
+        enumBuilder.addMethod(methodSpec);
+
+        pojoBuilder.addType(enumBuilder.build());
+    }
 
     @Override
     public TypeSpec generate() {
@@ -28,6 +78,11 @@ public class PxPojoTypeSpec extends AbstractPojoTypeSpec {
                     .addJavadoc(td.comment());
 
             pojoBuilder.addField(fieldBuilder.build());
+
+            if ("status".contains(td.field()) || "Status".contains(td.field()) ||
+                    "type".contains(td.field()) || "Type".contains(td.field())) {
+                buildEnum(td, pojoBuilder);
+            }
         });
 
         annotationSpecs.stream()
