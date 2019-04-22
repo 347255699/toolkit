63b47ffdcc609075c9e63d3b9ef520508ba3ae2e
menfre <menfre@wokoworks.com>
2019-04-22 18:50:59 +0800

    格式化代码

--- web/src/main/java/org/mendora/WebModule.java
+++ web/src/main/java/org/mendora/WebModule.java
@@ -20,7 +20,7 @@ public class WebModule {
 	public void run(Vertx vertx, String basePackageName) {
 		PackageScanner<RouteFactory> scanner = new PackageScannerImpl<>(basePackageName, WebModule.class.getClassLoader());
 		List<RouteFactory> routeFactories = scanner.newInstances(scanner.fullClassNames(RouteFactory.class.getName()), Route.class);
-		WebVerticle webVerticle = new WebVerticle(routeFactories, port == 0? 8080:port);
+		WebVerticle webVerticle = new WebVerticle(routeFactories, port == 0 ? 8080 : port);
 		DeploymentOptions deploymentOptions = new DeploymentOptions()
 			.setWorker(worker);
 		vertx.deployVerticle(webVerticle, deploymentOptions);
